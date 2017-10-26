e33a0af418e0af5f412e135a820bf66d744c65a6
fengtianyu <fengtianyu@syswin.com>
2017-10-26 17:15:16 +0800

    初始化一个collectionView

--- PhotoSyncAssistant/ViewController.swift
+++ PhotoSyncAssistant/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
 
+    let kCellMargin = 2
+    let kLineMargin = 2
+    let kCellCountInLine = 4
+    let kPhotoCollectionViewCellId:String = "PhotoCollectionViewCellId"
+    
+    //MARK: Property
+    lazy var collectionView:UICollectionView = {
+        
+        var itemSize = self.calculateItemSize()
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: itemSize, height: itemSize)
+        layout.scrollDirection = UICollectionViewScrollDirection.vertical
+        layout.minimumLineSpacing = 2
+        layout.minimumInteritemSpacing = 2
+        
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        return collectionView
+    }()
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.title = "照片"
+        
+        self.view.addSubview(self.collectionView)
+        self.collectionView.frame = CGRect(x: 0, y: 0, width:UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+        self.collectionView.backgroundColor = UIColor.white
+        self.collectionView.delegate = self as UICollectionViewDelegate
+        self.collectionView.dataSource = self as UICollectionViewDataSource
+        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: kPhotoCollectionViewCellId)
+    }
+    
+    // MARK: Private Method
+    func calculateItemSize() -> Int {
+        let screenWidth = UIScreen.main.bounds.size.width
+        let itemWidth = (Int(screenWidth)-kCellMargin*(kCellCountInLine-1)) / kCellCountInLine
+        
+        return itemWidth
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Delegate
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kPhotoCollectionViewCellId, for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
 }
 
